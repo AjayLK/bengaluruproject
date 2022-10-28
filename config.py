1e15e687dbaa1f24f11bb46a708b706d2368a82f
Ajay <ajaykshirsagar500@gmail.com>
2022-10-28 23:19:52 +0530

    project configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+
+import os
+
+PORT_NUMBER = 5055
+
+PICKLE_FILE_PATH = os.path.join("Bengaluru_house_data","bengaluru_model.pkl")
+
+JSON_FILE_PATH = os.path.join("Bengaluru_house_data","json_bengaluru_data.json")
